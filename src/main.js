aee80f5740b7cfb182e19e39e1384ec4cbff1c01
ennea8 <ennea8@gmail.com>
2020-04-30 18:57:33 +0800

    Add packages

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 import App from './App.vue'
 import router from './router'
+
+Vue.use(ElementUI)
 
 window.blockstack = require('blockstack')
 window.axios = require('axios')
