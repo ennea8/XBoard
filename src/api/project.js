6ac442b3ff8885df78ac87d6442d7d47baa458a5
ennea8 <ennea8@gmail.com>
2020-04-30 23:44:46 +0800

    Int API structure

---
+++ src/api/project.js
@@ -0,0 +1,24 @@
+export default {
+
+  // 创建一个项目，同时创建一个group与之关联
+  create (name) {
+
+  },
+  // 获取我加入的包括创建的
+  getMyProjects () {
+
+  },
+  // 获取我创建的
+  getMyCreatedProjects () {
+
+  },
+
+  //
+  invite (projectId, blockStackIdToInvite) {
+
+  },
+  active (invitingCode) {
+
+  }
+
+}
