3fcc674467ccdb69a943e41cd4e24159e511b05d
ennea8 <ennea8@gmail.com>
2020-04-30 18:57:57 +0800

    Opt UI

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,21 +1,15 @@
 <template>
-  <div class="panel-welcome" id="section-2">
-    <div class="avatar-section">
-      <img :src="avatar" class="img-rounded avatar" id="avatar-image">
-    </div>
+  <div class="nav">
+    <!--<div class="avatar-section">-->
+      <!--<img :src="avatar" class="img-rounded avatar" id="avatar-image">-->
+    <!--</div>-->
     <!--<h1 class="landing-heading">Hello, <span id="heading-name">{{ givenName }}</span>!</h1>-->
-    <h1 class="landing-heading">Welcome XBoard </span>!</h1>
+    <div class="app-info">XBoard </div>
 
-    <p class="lead">
-      <a
-        href="#"
-        class="btn btn-primary btn-lg"
-        id="signout-button"
-        @click.prevent="signOut"
-      >
-        Logout
-      </a>
-    </p>
+    <div class="right-area">
+      <img :src="avatar" class="img-rounded avatar" id="avatar-image">
+      <el-button type="text" @click.prevent="signOut">Logout</el-button>
+    </div>
   </div>
 </template>
 
@@ -51,4 +45,29 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+  .nav{
+    height: 50px;
+    overflow: hidden;
+    position: relative;
+    background: #fff;
+    padding: 0 10px 0 10px;
+    box-shadow: 0 1px 4px rgba(0,21,41,.08);
+    .app-info{
+      float: left;
+      line-height: 50px;
+    }
+    .right-area{
+      float:right;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 50px;
+
+      .avatar{
+        width: 30px;
+        height: 30px;
+      }
+    }
+  }
+</style>
--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="panel-landing" id="section-1">
-    <h1 class="landing-heading">Hello, Blockstack!</h1>
-    <p class="lead">
-      <a
-        href="#"
-        class="btn btn-primary btn-lg"
-        id="signin-button"
-        @click.prevent="signIn"
-      >
-        Sign In with Blockstack
-      </a>
-    </p>
+  <div class="center">
+    <div class="panel-landing" id="section-1">
+      <h1 class="landing-heading">Wecome, XBoard!</h1>
+      <p class="lead">
+        <a
+                href="#"
+                class="btn btn-primary btn-lg"
+                id="signin-button"
+                @click.prevent="signIn"
+        >
+          Sign In with Blockstack
+        </a>
+      </p>
+    </div>
   </div>
+
 </template>
 
 <script>
